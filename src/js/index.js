40a55b5bd2e0a8b7a85344ad96b6499941cd7a3d
Sulton <sulton.xojaniyazov@gmail.com>
2023-09-09 22:08:58 +0500

    index page complete

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+const burgermenuSwitch = () => {
+    document.querySelector(".burgermenu").classList.toggle("burgermenu_active");
+    document.querySelector(".nav").classList.toggle("nav_active");
+}
